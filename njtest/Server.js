dba7d6cd367e63aa3210bb6d3ab61699e09919a9
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-07-06 22:19:23 -0500

    project-folder + spotify repo

--- njtest/Server.js
+++ njtest/Server.js
@@ -8,3 +8,14 @@
     response.write("Hello World");
     response.end();
   }).listen(8888);
+
+  // to see if it runs: 
+  // cd into the right directory
+  // run `node server.js`
+  // then open up this url: localhost:8888
+  // you shoud see 'Hello World' printed on the document
+  //  ctrl-c to quit server
+
+  // Note: In the endpoint reference pages, see cURL syntax for the example requests. You do not need cURL to complete this tutorial, but you will probably find it a helpful tool when developing your application; it will help you formulate requests to Spotify web services and see the responses they return. Install the version with SSL support if you need it.
+
+  
